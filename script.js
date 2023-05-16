a4bd787a82ff1ffb21bc617e1fceb232a0232a1e
Kris <120410235+Kris1027@users.noreply.github.com>
2023-05-16 21:59:23 +0200

    add toDoList

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const wrapper = document.querySelector('.wrapper');
 
-window.addEventListener('mousemove', function () {
+window.addEventListener('mousemove', () => {
     wrapper.classList.add('active');
 })
+
+const input = document.querySelector('.todolist__text');
+const btn = document.querySelector('.todolist');
+const list = document.querySelector('.todolist__list');
+
+const addList = (e) => {
+    e.preventDefault();
+    const listItem = document.createElement('li');
+    list.appendChild(listItem)
+    listItem.classList.add('listStyle')
+    listItem.textContent = input.value;
+    input.value = '';
+
+    listItem.addEventListener('click', () => {
+        listItem.style.color = 'red';
+        listItem.style.textDecoration = 'line-through';
+    })
+
+    listItem.addEventListener('dblclick', () => {
+        listItem.style.textDecoration = 'none';
+        listItem.style.color = 'white';
+    })
+}
+
+btn.addEventListener('submit', addList);
